72fd3fe0b61b0dbb9a8efff7b1ab4c4a67276420
TJK98 <wtj1998@naver.com>
2025-08-28 16:40:12 +0900

    Add initial Spring Boot project configuration

---
+++ src/main/java/com/aid/train/backend/TrAInBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aid.train.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrAInBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrAInBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aid/train/backend/TrAInBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aid.train.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrAInBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
