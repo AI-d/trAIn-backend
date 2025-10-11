32b3c2e687f86d19c8848c1929483bd4ce37b2a9
dohee <doheeeq@gmail.com>
2025-10-11 23:22:33 +0900

    TRAIN-19 feat: API 공통 응답 엔드포인트 생성 (#9)
    * feat: Scenario 엔터티, 리포지토리 생성
    
    * feat: Scenario 개별 조회, 전체 조회 리포지토리 테스트
    
    * feat: queryDsl, dto 검증을 위한 validation 의존성 추가, 사용자 생성 시나리오 처리를 위한 dto, 편의 메소드 추가, 사용자 생성 시나리오 리포지토리 테스트
    
    * TRAIN-16 Scenario 엔터티 생성, Repository 테스트
    
    * feat: scenario 컨트롤러 작성, api 테스트
    
    * feat: build.gradle 의존성 추가
    
    * TRAIN-17 feat: querydsl 사용을 위한 의존성 추가
    
    * TRAIN-17 feat: scenario 엔터티, 리포지토리 생성 및 테스트 완료, 컨트롤러 생성 api 테스트 완료
    
    * TRAIN-17 feat: scenario 엔터티, 리포지토리 생성 및 테스트 완료, 컨트롤러 생성 api 테스트 완료
    
    * TRAIN-17 feat: scenario 엔터티, 리포지토리 생성 및 테스트 완료, 컨트롤러 생성 api 테스트 완료
    
    * TRAIN-17 feat: API 테스트를 위해 Scenario 엔터티, User 엔터티에 JsonIgnore 추가
    
    * TRAIN-19 feat: API 공통 응답 엔드포인트 생성

--- src/main/java/com/aid/train/backend/controller/scenario/ScenarioController.java
+++ src/main/java/com/aid/train/backend/controller/scenario/ScenarioController.java
@@ -3,6 +3,7 @@ package com.aid.train.backend.controller.scenario;
 import com.aid.train.backend.domain.scenario.dto.request.ScenarioRequestDto;
 import com.aid.train.backend.domain.scenario.entity.Scenario;
 import com.aid.train.backend.domain.user.entity.User;
+import com.aid.train.backend.global.common.response.ApiResponse;
 import com.aid.train.backend.repository.scenario.ScenarioRepository;
 import com.aid.train.backend.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,13 +25,13 @@ public class ScenarioController {
     @GetMapping("/{id}")
     public ResponseEntity<?> findScenario(@PathVariable Long id) {
         Scenario scenario = scenarioRepository.findScenarioByScenarioId(id);
-        return ResponseEntity.ok().body(scenario);
+        return ResponseEntity.ok().body(ApiResponse.success(id + "번 시나리오 조회에 성공했습니다.", scenario));
     }
 
     @GetMapping
     public ResponseEntity<?> findAllScenarios() {
         List<Scenario> allScenario = scenarioRepository.findAllScenario();
-        return ResponseEntity.ok().body(allScenario);
+        return ResponseEntity.ok().body(ApiResponse.success("전체 시나리오 조회를 성공했습니다.", allScenario));
     }
 
     @PutMapping
@@ -38,7 +39,7 @@ public class ScenarioController {
         User user = userRepository.findById(dto.ownerId()).orElseThrow();
         Scenario entity = Scenario.toEntity(dto,user);
         Scenario saved = scenarioRepository.save(entity);
-        return ResponseEntity.ok().body(saved);
+        return ResponseEntity.ok().body(ApiResponse.success(user.getName() + "님의 시나리오 생성을 성공했습니다.", saved));
     }
 
 }
