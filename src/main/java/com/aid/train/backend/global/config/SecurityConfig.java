122b49a759036d1e2da656c9ff00be6bc9d3fbd8
dohee <doheeeq@gmail.com>
2025-10-20 10:34:50 +0900

    TRAIN-63 feat: CorsConfig 설정 파일 추가, SecurityConfig cors 설정 추가 (#24)

--- src/main/java/com/aid/train/backend/global/config/SecurityConfig.java
+++ src/main/java/com/aid/train/backend/global/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package com.aid.train.backend.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // CSRF 비활성화 (WebSocket 사용 시 필수)
                 .csrf(AbstractHttpConfigurer::disable)
+
+                // cors 설정
+                .cors(cors -> cors.configure(http))
 
                 // 요청 권한 설정
                 .authorizeHttpRequests(auth -> auth
